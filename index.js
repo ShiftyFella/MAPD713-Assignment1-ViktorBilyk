defb7afdfaf9d5183614679bbe16f92c6fad3289
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-10-03 13:59:05 -0400

    server created server starts listening at defined HOST:PORT location

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ var postRequestCounter = 0;
 //required dependencies
 var restify = require('restify');
 var productsInMemDB = require('save')('products');
+
+//creating server
+server = restify.createServer({ name: SERVER_NAME});
+
+//starting server at desired Host:Port
+server.listen(PORT, HOST, function () {
+    console.log("Server %s started listening at %s", server.name, server.url);
+    console.log("Avaliable endpoints:");
+    console.log("%s/sendGet  --- Shows all products", server.url);
+    console.log("%s/sendPost --- Add products in format {id,product: name,price: price}", server.url);
+    console.log("%s/sendDelete --- Deletes all products", server.url);
+});
